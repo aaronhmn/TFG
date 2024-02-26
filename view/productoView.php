098cf711e2ad05cb4d2f8748694bcf327c06b865
aaronhelices@gmail.com <aaronhelices@gmail.com>
2024-02-26 19:33:49 +0100

    cambios de diseño de bootstrap en descripcion

--- view/productoView.php
+++ view/productoView.php
@@ -417,6 +417,7 @@ namespace views;
   }
   ?>
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
   <script src="../assets/js/producto.js"></script>
