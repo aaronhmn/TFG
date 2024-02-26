d6864f9defc4b96e791de7837616b243e247c896
aaronhelices@gmail.com <aaronhelices@gmail.com>
2024-02-26 21:07:29 +0100

    mejora en comentarios del js

--- assets/js/producto.js
+++ assets/js/producto.js
@@ -1,13 +1,13 @@
   //*Función para desplazarse suavemente hacia arriba
   function scrollToTop() {
-    //*Desplazamiento suave hacia arriba
+    //?Desplazamiento suave hacia arriba
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   }
 
-  //*Mostrar/ocultar el botón al hacer scroll
+  //?Mostrar/ocultar el botón al hacer scroll
   window.onscroll = function() {
     scrollFunction();
   };
@@ -21,17 +21,22 @@
     }
   }
 
-  //*Transform value
+  //?Transform value
   let value = 0
-  //*trail index number
+  //?trail index number
   let trailValue = 0
-  //*interval (Duration)
+  //?interval (Duration)
   let interval = 4000
 
+
   //*Galeria Producto
+  //?recogo la id de la imagen grande
   let mainImg = document.getElementById('mainImg');
+
+  //?recogo las imagenes pequeñas mediante sus clases de css
   let smallImg = document.getElementsByClassName('small-img');
 
+  //?Para cambiar la imagen grande por la imagen pequeña a la que haga le click
   smallImg[0].onclick = function(){
     mainImg.src = smallImg[0].src;
   }
@@ -48,15 +53,18 @@
     mainImg.src = smallImg[3].src;
   }
 
-  // Obtener todos los elementos con la clase 'small-img-col'
+ //*Dar borde a la imagen seleccionada para informacion de que imagen esta seleccionando para ampliarla
+ //?Obtener todos los elementos con la clase 'small-img-col'
   const smallImgCols = document.querySelectorAll('.small-img-col');
 
-  // Iterar sobre cada elemento y agregar un evento de clic
+  //?Iterar sobre cada elemento y agregar un evento de click
   smallImgCols.forEach(col => {
     col.addEventListener('click', () => {
-      // Eliminar la clase 'selected' de todos los elementos
+      
+      //?Eliminar la clase 'selected' de todos los elementos
       smallImgCols.forEach(col => col.classList.remove('selected'));
-      // Agregar la clase 'selected' solo al elemento clicado
+
+      //?Agregar la clase 'selected' solo al elemento clicado
       col.classList.add('selected');
     });
   });
