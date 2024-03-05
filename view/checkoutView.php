5b9ebc28e9174e0349d97e70bf4a78f27bf140c6
aaronhelices@gmail.com <aaronhelices@gmail.com>
2024-03-05 13:48:06 +0100

    modificacion del checkout

--- view/checkoutView.php
+++ view/checkoutView.php
@@ -7,7 +7,7 @@ namespace views;
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../assets/styles/css/contacto.css" />
+    <link rel="stylesheet" href="../assets/styles/css/style.css" />
     <link rel="icon" type="image/vnd.icon" href="../assets/img/genesis logo sin fondo favicon.png">
     <link
       rel="stylesheet"
@@ -359,5 +359,6 @@ namespace views;
       integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
       crossorigin="anonymous"
     ></script>
+    <script src="../assets/js/main.js"></script>
   </body>
 </html>
